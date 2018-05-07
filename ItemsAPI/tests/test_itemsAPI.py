e6e2ccaf5d4a4e28124f9c2ddd2370e8e945a4f4
Airmagic <babaphat@yahoo.com>
2018-05-07 11:51:21 -0500

    working on test, hard to do json files, also change gitignore file to not include test pycache

--- ItemsAPI/tests/test_itemsAPI.py
+++ ItemsAPI/tests/test_itemsAPI.py
@@ -1,14 +1,44 @@
 # Python batteries included
-import unittest
+from unittest import TestCase
+import os
+
 # importing itemsAPI program that Eric made
 from ItemsAPI import add_item, get_item, item_detail, item_update, item_delete
+import ItemsAPI
+
+
+# These are pip installs
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+# Binding these to the Flask
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 # Making a class so the unit testing can use
-class TestItemsAPI(unittest.TestCase):
+class TestItemsAPI(TestCase):
+
+    test_db_url = 'test_itemsAPI.db'
+
+
+    def setUp(self):
+        # TODO: Create temp db to use
+        ItemsAPI.db = self.test_db_url
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'items.sqlite')
+
+
+        db.session.commit()
 
     def test_add_item(self):
         # TODO: make a test and comment out pass
-        pass
+        item = ItemsAPI.Item("", "", "", "", "", "", "", "", "", "", "")
+        # item = ItemsAPI.Item({"cost": "","itemName": "", "location": "", "user": "", "website": "", "whenBarrowed": "", "whenBought": "", "whenReturned": "", "whereBarrowed": "", "whereBought": "", "whoBarrowed": ""})
+        ItemsAPI.item_schema.jsonify(item)
+        expected = {"cost": "","itemName": "", "location": "", "user": "", "website": "", "whenBarrowed": "", "whenBought": "", "whenReturned": "", "whereBarrowed": "", "whereBought": "", "whoBarrowed": ""}
+        self.compare_db_expected(expected )
+        # pass
 
     def test_get_item(self):
         # TODO: make a test and comment out pass
@@ -25,3 +55,19 @@ class TestItemsAPI(unittest.TestCase):
     def test_item_delete(self):
         # TODO: make a test and comment out pass
         pass
+
+
+    def compare_db_to_expected(self,expected):
+        id = request.json['id']
+        user =request.json['user']
+        itemName = request.json['itemName']
+        location = request.json['location']
+        whereBought = request.json['whereBought']
+        whenBought = request.json['whenBought']
+        cost = request.json['cost']
+        website = request.json['website']
+        whoBarrowed = request.json['whoBarrowed']
+        whenBarrowed = request.json['whenBarrowed']
+        whenReturned = request.json['whenReturned']
+        whereBarrowed = request.json['whereBarrowed']
+        # Same rows in DB as entries in expected dictionary

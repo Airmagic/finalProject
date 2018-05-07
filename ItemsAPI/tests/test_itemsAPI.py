1d0f81dfb61ca803bb8f5de202660b4cc02e0d4e
Airmagic <babaphat@yahoo.com>
2018-05-07 10:27:22 -0500

    beginning of the test document

---
+++ ItemsAPI/tests/test_itemsAPI.py
@@ -0,0 +1,27 @@
+# Python batteries included
+import unittest
+# importing itemsAPI program that Eric made
+from ItemsAPI import add_item, get_item, item_detail, item_update, item_delete
+
+# Making a class so the unit testing can use
+class TestItemsAPI(unittest.TestCase):
+
+    def test_add_item(self):
+        # TODO: make a test and comment out pass
+        pass
+
+    def test_get_item(self):
+        # TODO: make a test and comment out pass
+        pass
+
+    def test_item_detail(self):
+        # TODO: make a test and comment out pass
+        pass
+
+    def test_item_update(self):
+        # TODO: make a test and comment out pass
+        pass
+
+    def test_item_delete(self):
+        # TODO: make a test and comment out pass
+        pass
